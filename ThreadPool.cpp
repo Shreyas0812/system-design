f912916ea830a431e70860bf889d274401e06814
Shreyas Raorane <shreyasraorane0812@gmail.com>
2026-08-30 22:59:42 -0400

    ThreadPool implementation to keep a fixed number of threads

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -15,8 +15,71 @@ Implementation of ThreadPool -- we define the number of threads so we can avoid 
 
 class ThreadPool {
 public:
-    ThreadPool(size_t num_workers);
-    ~ThreadPool();
+    ThreadPool(size_t num_workers) {
+        for (size_t i = 0; i < num_workers; ++i) {
+            workers_.emplace_back([this] { worker_loop(); });
+        }
+    }
+    ~ThreadPool() {
+        {
+            std::unique_lock<std::mutex> lock(mtx_);
+            stop_ = true;
+        }
+        cv_.notify_all();
+        for (auto& worker : workers_) {
+            if (worker.joinable()) {
+                worker.join();
+            }
+        }
+    };
 
-    
+    void enqueue(std::function<void()> task) {
+        {
+            std::unique_lock<std::mutex> lock(mtx_);
+            tasks_.push(std::move(task));
+        }
+        cv_.notify_one();
+    }
+
+    //No copying 
+    ThreadPool(const ThreadPool&) = delete;
+    ThreadPool& operator=(const ThreadPool&) = delete;
+private:
+    void worker_loop() {
+        while (true) {
+            std::function<void()> task;
+            {
+                std::unique_lock<std::mutex> lock(mtx_);
+                cv_.wait(lock, [this] { return !tasks_.empty() || stop_; });
+                if (stop_ && tasks_.empty()) {
+                    return;
+                }
+                task = std::move(tasks_.front());
+                tasks_.pop();   
+            }
+            std::cout << "Thread " << std::this_thread::get_id() << " executing task" << std::endl;
+            task();
+        }
+    }
+
+    std::vector<std::thread> workers_;
+    std::queue<std::function<void()>> tasks_;
+    std::mutex mtx_;
+    std::condition_variable cv_;
+    bool stop_ = false;
 };
+
+int main() {
+    ThreadPool pool(4); // Create a thread pool with 4 worker threads
+
+    for (int i = 0; i < 10; ++i) {
+        pool.enqueue([i] {
+            std::cout << "Task" << i << " on thread "
+                    << std::this_thread::get_id() << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        });
+    } // pool destructor called here
+
+    std::cout << "All Done" << std::endl;
+    return 0;
+}
